870dbb5d8ec477a4a33aab148425db6c808eba93
Tyler <tyler@anymod.com>
2020-11-05 18:21:48 -0800

    Update test setup

--- test/reactdom.render.spec.js
+++ test/reactdom.render.spec.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Toolkit from "../src/index.js";
+import { utils } from "@anymod/core";
+
+const scope = {};
 
 const Signup = Toolkit.signupForm({
   tenantId: "A1B2",
@@ -18,15 +21,17 @@ function App() {
 describe("Render a signup form", () => {
   beforeAll(() => {
     document.body.innerHTML = '<div id="root"></div>';
-    // scope.modCb = jest.fn();
-    // const tempFn = utils.loadPageAssets;
-    // utils.loadPageAssets = (a) => {
-    //   return tempFn(a, { scriptTag, styleTag, modCb: scope.modCb });
-    // };
+    scope.modCb = jest.fn();
+    const tempFn = utils.loadPageAssets;
+    utils.loadPageAssets = (a) => {
+      return tempFn(a, { scriptTag, styleTag, modCb: scope.modCb });
+    };
   });
 
   it("should render a signup component", () => {
     ReactDOM.render(<App />, document.getElementById("root"));
+    // TODO update this assertion to contain the HTML associated with
+    // The mod that should be rendered into this location.
     expect(document.body.innerHTML).toContain(
       `<div id="userfront-abcdefg"></div>`
     );
