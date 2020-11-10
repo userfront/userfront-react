544828ab9dc65fa59ca69c04aba65a6fea4f788b
Tyler <tyler@anymod.com>
2020-11-09 21:34:42 -0800

    Getting react to render

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import core from "@anymod/core";
 const {
   Singleton,
+  alias,
   processPage,
   createOrReturnPage,
   checkPageAndUpdate,
@@ -12,11 +13,11 @@ const {
 
 async function pager() {
   try {
-    console.log(document.body.innerHTML);
-    console.log(Singleton);
     const page = await createOrReturnPage();
     const updatedPage = await checkPageAndUpdate(page);
+    console.log("Start processing");
     await processPage(updatedPage);
+    console.log("Done processing");
     executeCallbacks();
     logErrorsAndTips();
     setTimeout(uncloakNonMods, 1);
@@ -28,16 +29,9 @@ async function pager() {
 }
 
 const Toolkit = {
-  /**
-   * TODO
-   * Add processPage into the method that gets returned
-   * by signupForm (or other toolkit exports).  The same render chain
-   * should be usable to determine find/create the page and then render
-   * it into the component after it has been inserted into the DOM
-   */
-  async componentDidMount() {},
-
   signupForm({ tenantId, toolId }) {
+    alias.setAlias("Userfront");
+    Singleton.Opts.api = true;
     return function Signup() {
       useEffect(() => {
         pager();
--- test/reactdom.render.spec.js
+++ test/reactdom.render.spec.js
@@ -2,32 +2,53 @@ import React from "react";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import Test from "./config/test.utils.js";
 import core from "@anymod/core";
-const { utils } = core;
+const { Singleton, utils, crud } = core;
 
 import Toolkit from "../src/index.js";
 
 const scope = {};
 
 const Signup = Toolkit.signupForm({
-  tenantId: "A1B2",
+  tenantId: "FOOBAR",
   toolId: Test.factories.mods.basic.key,
 });
 
 describe("Render a signup form", () => {
   beforeAll(() => {
     scope.modCb = jest.fn();
-    const tempFn = utils.loadPageAssets;
-    utils.loadPageAssets = (a) => {
-      console.log("loadPageAssets");
-      return tempFn(a, { scriptTag, styleTag, modCb: scope.modCb });
+    const tempLoadFn = utils.loadPageAssets;
+    utils.loadPageAssets = (a, b) => {
+      return tempLoadFn(a, {
+        modCb: scope.modCb,
+        ...b,
+      });
+    };
+    scope.postFn = jest.fn();
+    crud.post = async (a) => {
+      scope.postFn(a);
+      return Test.factories.pages.basic;
     };
   });
 
-  it("should render a signup component", async () => {
+  it("should render a signup form and its assets if no page exists yet", async () => {
+    Singleton.Opts.debug = true;
     render(<Signup />);
-    expect(scope.modCb).toHaveBeenCalled();
+    await waitFor(() => {
+      expect(scope.postFn).toHaveBeenCalled();
+    });
+    Test.fns.fireAllOnloads();
+    await waitFor(() => {
+      expect(scope.modCb).toHaveBeenCalled();
+      expect(document.body.innerHTML).toContain(Test.factories.mods.basic.html);
+    });
+    console.log("head", document.head.innerHTML);
+    console.log("body", document.body.innerHTML);
+    console.log("here");
+    expect(document.head.innerHTML).toContain(Test.factories.mods.basic.css);
+    expect(document.body.innerHTML).toContain(Test.factories.mods.basic.html);
     expect(document.body.innerHTML).toContain(
       `<div id="userfront-${Test.factories.mods.basic.key}"></div>`
     );
+    expect(scope.modCb).toHaveBeenCalled();
   });
 });
